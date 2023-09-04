421aaccdda0811a363e436949606bd273f85ef09
Matias.E.Garcia <matiasegarcia22@gmail.com>
2023-09-04 20:54:46 -0300

    useForm

--- src/Components/Access.jsx
+++ src/Components/Access.jsx
@@ -1,15 +1,56 @@
-import {PersonFillAdd, PersonUp} from "react-bootstrap-icons"; 
+import { PersonFillAdd, PersonUp } from "react-bootstrap-icons";
 import { PASSWORD_LABEL, USERNAME_LABEL } from "../Util/UtilTexts";
+import { useForm } from "../hooks/useForm";
+
+/**
+ * initial values in the authenticationForm inputs
+ */
+const initialForm = {
+    username: "",
+    password: "",
+}
+/**
+ * Function to validate each input in the authenticationForm.
+ * @param {Object} form - object that will contain inputs values to test and see if there is any error.
+ * @returns {Object} - in the case that there was any error will have input name and its error messages.
+ */
+const validationsForm = (form) => {
+    let errors = {};
+    if (!form.username.trim()) {
+        errors.username = "Please , add a username";
+    } else if (!/^\S{1,20}$/.test(form.username)) {
+        errors.username = "Username can't contain spaces or be more than 20 characters";
+    }
+
+    if (!form.password.trim()) {
+        errors.password = "Please , add a password";
+    } else if (!/^\S{1,100}$/.test(form.password)) {
+        errors.password = "Username can't contain spaces or be more than 100 characters";
+    }
+    return errors;
+}
+
 /**
  * 
  * @param {string} props.typeOfAccess - depend of the type of the access that the user wants, the authenticationForm will sign up or sign in the user.
  * @returns {JSX.Element} - The rendered AccessForm component.
  */
-function Access({typeOfAccess}) {
+function Access({ typeOfAccess }) {
+    const { form,
+        fieldsTouched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit
+    } = useForm(initialForm, validationsForm);
+
+    function authSubmit(evt) {
+        console.log('formulario enviado');
+    }
 
     return (
         <main className="container-fluid d-flex justify-content-center align-items-center vh-100">
-            <form action="" className="w-75 w-md-25">
+            <form className="w-75 w-md-25 needs-validation" noValidate onSubmit={evt => handleSubmit(evt, authSubmit)}>
                 <div className="row">
                     <div className="col-12 text-center">
                         {typeOfAccess === 'signIn'
@@ -19,29 +60,43 @@ function Access({typeOfAccess}) {
                 </div>
                 <div className="row gap-3">
                     <div className="col-12 text-center">
-                        <label for="username" classname="form-label">
+                        <label htmlFor="username">
                             <p className="fs-4">{USERNAME_LABEL}</p>
                         </label>
                         <input
+                            required
                             type="text"
                             className="form-control"
                             id="username"
                             name="username"
-                            placeholder="---" />
+                            value={form.username}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        <small className={`${fieldsTouched.username && !!errors.username ? "d-block": "d-none"} text-danger `}>
+                            {errors.username}
+                        </small>
                     </div>
                     <div className="col-12 text-center">
-                        <label for="password" classname="form-label">
+                        <label htmlFor="password">
                             <p className="fs-4">{PASSWORD_LABEL}</p>
                         </label>
                         <input
+                            required
                             type="password"
                             className="form-control"
                             id="password"
                             name="password"
-                            placeholder="---" />
+                            value={form.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        <small className={`${fieldsTouched.password && !!errors.password ? "d-block": "d-none"} text-danger`}>
+                            {errors.password}
+                        </small>
                     </div>
                     <div className="col-12 text-center">
-                        <button type="submit" class="btn btn-success w-50">
+                        <button type="submit" className="btn btn-success w-50">
                             Submit
                         </button>
                     </div>
---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+
+/**
+ * useHook to validate forms.
+ * @param {Object} initialForm - Will contain the initial values in the inputs forms.
+ * @param {Function} validateForm - Function that will validate inputs values in forms.
+ * @returns {Object} - Object that will contain neccesary methods to work with this useHook and validate the form.
+ */
+export const useForm = (initialForm, validateForm) => {
+    const [form, setForm] = useState(initialForm);
+    const [errors, setErrors] = useState({});
+    const [fieldsTouched, setFieldsTouched] = useState({});
+
+    function handleTouched(fieldTouched){
+        setFieldsTouched({
+            ...fieldsTouched,
+            [fieldTouched]:true
+        });
+    }
+
+    function handleChange(evt){
+        const{name, value} = evt.target;
+        setForm({
+            ...form,
+            [name]:value,
+        });
+    }
+
+    /**
+     * Function to set wich input in the form was already touched, to set errors in the form and handle any change in the inputs.
+     * @param {Event} evt - event that will came from an input.
+     */
+    function handleBlur(evt){
+        handleChange(evt);
+        handleTouched(evt.target.name);
+        setErrors(validateForm(form,fieldsTouched));
+    }
+
+    /**
+    * This method is only for prevent default behavior in the evt and to see  
+    * for last time if there is any error before send the form.
+    * @param {Event} evt - event that will came from the form.
+    * @param {Function} submit - submit function that will handle the event.
+    * @returns nothing if the form has errors, if not will execute {submit} passing {evt}.
+    */
+    function handleSubmit(evt,submit){
+        evt.preventDefault();
+        setErrors(validateForm(form,fieldsTouched));
+        //if there are no errors then I can submit
+        if(Object.keys(errors).length === 0){
+            submit(evt);
+        }else{
+            return;
+        }
+    }
+
+    return{form,
+            fieldsTouched,
+            errors,
+            handleChange,
+            handleBlur,
+            handleSubmit
+        }
+}
