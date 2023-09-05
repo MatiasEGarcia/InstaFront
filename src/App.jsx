5d87422a08f9daeaf65c313343cfd991ccc34982
Matias.E.Garcia <matiasegarcia22@gmail.com>
2023-09-05 13:28:08 -0300

    MainContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Access from "./Components/Access"
 import { SIGN_IN } from "./Util/UtilTexts"
+import MainContainer from "./Components/MainContainer"
 
 
 function App() {
   return (
     <>
-      <Access typeOfAccess={SIGN_IN}/>
+      {/*<Access typeOfAccess={SIGN_IN}/>*/}
+      <MainContainer/>
     </>
   )
 }
